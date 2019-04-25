7c48b4616dd4b0db1e81864ed313533a2baa0e67
jsmoot93 <47538435+jsmoot93@users.noreply.github.com>
2019-04-25 14:15:03 -0400

    added start script and dotenv changes to port

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 const server = require('./server.js');
 
-server.listen (5000, () => {
-    console.log('\n*** API running on port 5000 ***\n');
+const port = process.env.PORT || 5000
+server.listen (port, () => {
+    console.log(`\n*** API running on port ${port} ***\n`);
 });
